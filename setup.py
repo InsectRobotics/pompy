c78571cdd5d41d6e9a0a7378ee79f2af991724fe
Matt Graham <matthew.m.graham@gmail.com>
2016-10-21 01:56:27 +0100

    Reorganising into a package and adding setup.py script.

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""Setup script for pompy package."""
+
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(name='pompy',
+          description='Puff-based odour plume model',
+          author='Matt Graham',
+          url='https://github.com/InsectRobotics/pompy',
+          packages=['pompy'])
+
